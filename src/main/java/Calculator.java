59e4bfd7f5a9597209db9a7a1cf5726ea714fbe5
SilverSabre92 <git config --global user.name asz_login_do_githubnasz_login_do_github^Z>
2019-07-25 22:43:02 +0200

    Improvements

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,11 +4,21 @@ public class Calculator {
     int a;
     int b;
 
-    public int substractAFromB(int a, int b){
+    public Calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int substractAFromB(){
         return a-b;
     }
-    public int addAToB(int a, int b){
+    public int addAToB(){
         return a+b;
+    }
+    public static void main (String[] args) {
+        Calculator calculator = new Calculator (5,6);
+        Integer shazam = calculator.addAToB();
+        System.out.println(shazam);
     }
 }
 
