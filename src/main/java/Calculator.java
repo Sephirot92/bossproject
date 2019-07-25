df3cba820db8fe2c97f76cd33bd6780dcf560e41
SilverSabre92 <git config --global user.name asz_login_do_githubnasz_login_do_github^Z>
2019-07-25 21:45:45 +0200

    Task for boss

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    int a;
+    int b;
+
+    public int substractAFromB(){
+        return a-b;
+    }
+    public int addAToB(){
+        return a+b;
+    }
+}
---
+++ src/main/java/com/projectforboss/BossprojectApplication.java
@@ -0,0 +1,13 @@
+package com.projectforboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BossprojectApplication.class, args);
+    }
+
+}
